99b831da62352f7c64d91d70255607da560f9266
rafael <rafael201123r@gmail.com>
2024-12-06 14:13:06 -0300

    primeira versão do projeto, ainda sem o banco de dados, somente as telas

---
+++ ControleDeContatos/Controllers/ContatoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ConfirmarExcluir()
+        {
+            return View();
+        }
+    }
+}
